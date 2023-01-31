4af0f9290ba641a259b78af9b21efda19cc39cfb
fouzia <fouziasultana470@gmail.com>
2023-01-31 17:04:29 +0530

    javascript basics part one

---
+++ basics.js/strings.js
@@ -0,0 +1,17 @@
+"HELLO WORLD"
+'HELLO WORLD'
+`HELLO WORLD`
+
+"iam from \'Guntur\'"
+
+'Iam from \'Guntur'
+
+"Name:Fouzia city :guntur,designation:programmer"
+
+`
+Name:Fouzia
+City:Guntur
+Designation:programmer
+`
+'5 plus 5 is ${5+5}'
+
