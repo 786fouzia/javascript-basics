72f07e1a259ea314181a791e3efd61fe3a8dc250
fouzia <fouziasultana470@gmail.com>
2023-02-02 11:14:55 +0530

    array and object added

---
+++ 1. basics.js/2. Data Structure/1. array.js
@@ -0,0 +1,11 @@
+// arays start with []
+//array is used to define multiple values9(strings,boolens,numbers,array,object)
+
+[1, 2, 3, 4, 5];
+[1,2,3,4,5]
+[1,
+2,
+3,
+4,
+5];
+
---
+++ 1. basics.js/2. Data Structure/2. object.js
@@ -0,0 +1,1 @@
+{ "name"; "Md Fouzia", "designation"; "programmer" }
